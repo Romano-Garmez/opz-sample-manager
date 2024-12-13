9067795d8245799134d417b4524d9d8ae8dd2118
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2024-12-12 16:13:34 -0800

    progress

--- file-management.js
+++ file-management.js
@@ -3,6 +3,7 @@
 const { get } = require('http');
 
 let opzpath = "";
+const numslots = 10;
 
 const selectDriveBtn = document.getElementById('select-drive');
 const listFilesBtn = document.getElementById('list-files');
@@ -28,11 +29,27 @@ listFilesBtn.addEventListener('click', async () => {
     for (let sampletype in samplepackfolders) {
         console.log("current sample type: " + samplepackfolders[sampletype]);
         let samplepackdiv = document.getElementById(samplepackfolders[sampletype]);
-        console.log("samplepackdiv: " + samplepackdiv);
 
         if (samplepackdiv) {
             // If the element is found, update its innerHTML
-            samplepackdiv.innerHTML = listSamples(path.join(opzpath, 'samplepacks', samplepackfolders[sampletype])).join('<br>');
+
+            let samplelist = listSamples(path.join(opzpath, 'samplepacks', samplepackfolders[sampletype]));
+            //samplepackdiv.innerHTML = listSamples(path.join(opzpath, 'samplepacks', samplepackfolders[sampletype])).join('<br>');
+            console.log("samplelist: " + samplelist);
+
+            for (let i = 0; i < numslots; i += 1) {
+                let newdiv = document.createElement('div');
+                samplepackdiv.appendChild(newdiv);
+                newdiv.className = "sampleslot";
+                newdiv.id = i+1;
+
+                if (samplelist[i] != undefined) {
+                    newdiv.innerHTML = samplelist[i];
+                }
+                else {
+                    newdiv.innerHTML = "empty";
+                }
+            }
         } else {
             // Log if element is not found
             console.error("Element not found for sampletype:", samplepackfolders[sampletype]);
@@ -65,12 +82,17 @@ function listSamples(dirPath) {
 
     files.forEach(file => {
         const fullPath = path.join(dirPath, file);  // Get the full path of the current file/folder
+        console.log("fullPath: " + fullPath);
         const stats = fs.statSync(fullPath);  // Get stats to determine if it's a file or folder
 
         if (stats.isDirectory()) {
             // If it's a directory, recursively list files inside it
             sampleFiles = sampleFiles.concat(listSamples(fullPath));
         } else if (stats.isFile() && (file.endsWith('.aif') || file.endsWith('.aiff'))) {
+            //get slot #
+            let slot = fullPath.split('\\')[fullPath.split('\\').length - 2];
+            console.log("file: " + file + " slot: " + slot);
+
             // If it's a file and ends with .aif, add it to the sample list
             sampleFiles.push(file);
         }
