0316ca8ca3124f6276b646bd91903a7c8deb282a
Onyx Cheyne <75056546+alterednode@users.noreply.github.com>
2025-05-31 17:19:03 -0700

    get path from the right spot

--- static/js/configeditor.js
+++ static/js/configeditor.js
@@ -123,7 +123,7 @@ loadConfig(currentConfigName);
 async function pollForMount(retries = 60, delay = 2000) {
     for (let i = 0; i < retries; i++) {
         try {
-            const res = await fetch('/get-opz-mount-path');
+            const res = await fetch(`/get-config-setting?config_option="OPZ_MOUNT_PATH"`);
             const data = await res.json();
 
             if (data["OPZ_MOUNT_PATH"]) {
